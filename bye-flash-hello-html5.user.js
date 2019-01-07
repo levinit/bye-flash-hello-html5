9fc924133d9e5fe374352b6af59cc46d5f806908
levinit <levinit@outlook.com>
2019-01-07 21:28:32 +0800

    去掉爱奇艺和腾讯视频

--- bye-flash-hello-html5.user.js
+++ bye-flash-hello-html5.user.js
@@ -4,10 +4,6 @@
 // @author      levinit
 // @description 国内主流视频网站的HTML5播放
 // @include     *://*.le.com/*
-// @include     *://*.iqiyi.com/*html*
-// @include     *://v.qq.com/*
-// @include     *://y.qq.com/*
-// @include     *://view.inews.qq.com/*
 // @include     *://tv.cctv.com/*
 // @include     *://wlchunwan.cctv.com/*
 // @include     *://*.cntv.cn/video/*
@@ -44,7 +40,7 @@ if (location.href.indexOf('m.acfun') >= 0) {
 let [ua, isMobile] = [null, false]; //user-agent 和 是否使用移动ua
 
 //这些网站使用移动ua
-const sites = ['cctv', '.163', 'iqiyi', 'cntv', 'sohu', 'acfun'];
+const sites = ['cctv', '.163', 'cntv', 'sohu', 'acfun'];
 
 sites.forEach(curVal => {
   if (location.host.indexOf(curVal) >= 0) {
@@ -77,33 +73,15 @@ if (isMobile) {
 changeUA(ua);
 
 //=====
-//显示播放控制条 stuyd.163 iqiyi
-window.onload = function() {
+//显示播放控制条 stuyd.163
+window.onload = function () {
   if (
     isMobile === true &&
-    (location.href.search('study.163') >= 0 ||
-      location.href.search('iqiyi.com') >= 0)
+    location.href.search('study.163') >= 0
   ) {
     const v = ele('video');
     if (v) {
       v.setAttribute('controls', 'controls');
-      if (location.href.search('iqiyi.com') >= 0) {
-        iqiyi();
-      }
-    }
-  }
-  //爱奇艺的相关调整
-  function iqiyi() {
-    //爱奇艺默认的进度条 和 播放下一个视频和调整画质的控制条
-    const [defaultProcess, controlBar] = [
-      ele('.process-response'),
-      ele('.bottom')
-    ];
-    if (controlBar) {
-      controlBar.style.bottom = '25px'; //播放下一个视频和调整画质的控制条往上放
-      if (defaultProcess) {
-        defaultProcess.style.display = 'none'; //让默认进度条消失
-      }
     }
   }
 };
@@ -115,22 +93,6 @@ function ele(element) {
 }
 //更改ua的函数
 function changeUA(ua) {
-  //QQ音乐MV+Edge 不切换ua
-  if (
-    (location.host === 'y.qq.com') &&
-    navigator.userAgent.indexOf('Edge') >= 0
-  ) {
-    return false;
-  }
-
-  //爱奇艺+非Firefox 不切换ua
-  if (
-    location.host.indexOf('iqiyi') >= 0 &&
-    navigator.userAgent.indexOf('Firefox') === -1
-  ) {
-    return false;
-  }
-
   Object.defineProperty(navigator, 'userAgent', {
     value: ua,
     writable: false,
